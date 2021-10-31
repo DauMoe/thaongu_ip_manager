2dffb2fb0157d98996a01dd41530663e556405b2
DauMoe <lehuyhoang1111999@gmail.com>
2021-10-31 11:45:50 +0700

    ok

--- components/BlackList/BlackListServices.ts
+++ components/BlackList/BlackListServices.ts
@@ -10,14 +10,13 @@ export const NewBlackList: Function = async (req: Request, resp: Response): Prom
     try {
         //@ts-ignore
         let ip: string = reqData["ip"] as string;
-
         //@ts-ignore
         let desc: string = reqData["desc"] as string || undefined;
-
         //@ts-ignore
         let create_time: number = Number.parseInt(reqData["create_time"] as number) || undefined;
+
         CreateOneBlackList(ip, desc, create_time);
-        SuccessResp(resp, ["Success"]);
+        SuccessResp(resp);
     } catch(e) {
         C201Resp(resp, [e]);
     }
--- components/Utils/API_RESPONSE.ts
+++ components/Utils/API_RESPONSE.ts
@@ -1,6 +1,6 @@
 import {Response} from "express";
 
-export const RespCustomCode: Function = (resp: Response, code: number, data: any, isBreak: boolean = true): Response => {
+export const RespCustomCode: Function = (resp: Response, code: number, data: any): Response => {
     resp.statusCode = 200;
     resp.send({
         "code": code,
@@ -18,10 +18,13 @@ export const MissingField: Function = (resp: Response, field: string): Response 
     return resp;
 }
 
-export const SuccessResp: Function = (resp: Response, data: any, isBreak: boolean = true): Response => {
-    return RespCustomCode(resp, 200, data, isBreak);
+export const SuccessResp: Function = (resp: Response, data?: any, isBreak?: boolean): Response => {
+    if (data === undefined) {
+        return RespCustomCode(resp, 200, ["Success"]);
+    }
+    return RespCustomCode(resp, 200, data);
 }
 
-export const C201Resp: Function = (resp: Response, data: any, isBreak: boolean = true): Response => {
-    return RespCustomCode(resp, 201, data, isBreak);
+export const C201Resp: Function = (resp: Response, data: any): Response => {
+    return RespCustomCode(resp, 201, data);
 }
